4f68b8b9aeb5a736d06369723f533a743810a131
jianmei <shijianmei0@sina.com>
2018-09-18 20:21:22 +0800

    Initial Commit

---
+++ JMBadge/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JMBadge
+//
+//  Created by Jianmei on 2018/9/18.
+//  Copyright © 2018 Jianmei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
